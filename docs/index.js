558d2b9c86b0799f0ecab4fec234fc83888f209c
Aysa Badmaeva <aysabadmaeva012@gmail.com>
2024-12-03 12:21:23 +0300

    last build.

--- docs/index.js
+++ docs/index.js
@@ -1,15 +1,5 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  var image = new Image();
-  image.src = jpg;
-  document.querySelector('.images').appendChild(image);
-});
 /******/ })()
 ;
