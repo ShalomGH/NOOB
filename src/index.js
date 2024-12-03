ebba6b5337c2ea156b836b707065379f6084766b
Aysa Badmaeva <aysabadmaeva012@gmail.com>
2024-12-03 12:05:25 +0300

    add favicon and links

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 import './index.css'
-import './stylesheets/reset.scss'
-import './stylesheets/index.scss'
-import './stylesheets/vars.scss'
-import './stylesheets/fonts.scss'
-import './stylesheets/basics.scss'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const image = new Image()
-  image.src = jpg
-  document.querySelector('.images').appendChild(image)
-})
+
